1d4b209ac1c1f499b720885b7c4f5cb540679236
fede093 <fede.stark1@gmail.com>
2020-02-24 21:00:08 -0300

    Agrega funciones insertar review x juego

---
+++ DATOS/DatosReviewJuego.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTIDAD;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DATOS
+{
+    public class DatosReviewJuego
+    {
+        AccesoDatos ds = new AccesoDatos();
+
+        public bool InsertarReviewJuego(Review_Juego review_juego)
+        {
+            SqlConnection cn = ds.ObtenerConexion();
+
+            if (cn != null)
+            {
+                SqlCommand cmd;
+                String sql = "INSERT INTO RevXJue (Cod_Review, Cod_Usuario, Id_juego) values(" + 
+                    review_juego.cod_review + ", '" + review_juego.cod_usuario + "', " + review_juego.cod_juego +")";
+
+                cmd = new SqlCommand(sql, cn);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (SqlException ex)
+                {
+                    return false;
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ NEGOCIO/n_ReviewJuego.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTIDAD;
+using DATOS;
+using System.Data;
+
+namespace NEGOCIO
+{
+    public class n_ReviewJuego
+    {
+        public bool InsertarReviewJuego(Review_Juego review_juego)
+        {
+            DatosReviewJuego rj = new DatosReviewJuego();
+            return rj.InsertarReviewJuego(review_juego);
+        }
+    }
+}
